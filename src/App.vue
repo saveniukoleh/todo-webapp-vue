756e91baccb8714a998be1cb1ce5780fca606a50
Oleh Saveniuk <oleh.saveniuk@gmail.com>
2022-09-28 09:25:30 +0200

    Vuetify App home screen

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,23 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-toolbar-title>ToDo WebApp</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-btn class="mx-3" color="#1919d2" :to="{ name: 'event-list' }"
+        >List</v-btn
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-btn color="#19d2d2" :to="{ name: 'event-create' }">Create</v-btn>
     </v-app-bar>
 
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-main> <router-view /> </v-main>
+
+    <v-footer padless>
+      <v-card flat tile width="100%" class="primary lighten-1 text-center">
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>ToDo WebApp</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
@@ -44,9 +26,5 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "App",
-
-  data: () => ({
-    //
-  }),
 });
 </script>
