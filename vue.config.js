e2b58e66f3bdfb601c6ca78b676fad618378d718
Oleh Saveniuk <oleh.saveniuk@gmail.com>
2022-09-30 10:58:31 +0200

    Add cordova

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/todo-webapp-vue/" : "/",
+  publicPath: "",
+
+  pluginOptions: {
+    cordovaPath: "src-cordova",
+  },
 });
